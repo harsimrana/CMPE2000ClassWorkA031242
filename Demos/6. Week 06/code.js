83f7ca911f9f2196ab32601d6745c235415f8062
harsimrana <harsimrana@nait.ca>
2025-02-10 10:11:11 -0700

    Demo 13 and 14 commit

---
+++ Demos/6. Week 06/code.js
@@ -0,0 +1,57 @@
+// .js is extension of javasctip file
+
+function secondFunction()
+{
+    console.log("Inside second function");
+    document.body.style.backgroundColor="lightgreen";
+    alert("Color has been changed");
+}
+
+/************************************************************
+ * Function Name    : redirectToNait
+ * Purpose          : To redirect the user to NAIT Website
+ * Inputs           : No 
+ * Output           : NO
+ ************************************************************/
+function redirectToNait()
+{
+    console.log("Inside redirectToNait function");
+    window.location="https://www.nait.ca/nait/home";
+    // Location property - redirect the user to any other URL
+    // window keyword is optional because that is the default value
+}
+
+/************************************************************
+ * Function Name    : obtainDimensions
+ * Purpose          : To get dimensions of any window
+ * Inputs           : No 
+ * Output           : NO
+ ************************************************************/
+function obtainDimensions()
+{
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    if(width > 800 )
+    {
+        alert("Width of the window = " + width +" Height = " +height );
+    }
+    else
+    {
+        alert("Height Only = "+ height);
+    }
+
+}
+
+/************************************************************
+ * Function Name    : inputAge
+ * Purpose          : To get value from user 
+ * Inputs           : No 
+ * Output           : NO
+ ************************************************************/
+function inputAge()
+{
+    //           Prompt message   ,Default value
+    age = prompt("Enter your age", "0");
+    // It will return age value 
+}
