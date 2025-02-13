1970ac733eebdcb2d49db17648b7a3ea94969b34
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2025-02-12 17:49:49 -0700

    Almost done

--- Demos/6. Week 06/codedemo15.js
+++ Demos/6. Week 06/codedemo15.js
@@ -22,13 +22,41 @@ window.onload = function()
 
     //bind the element with id fisrtButton to event 
     document.getElementById("fisrtButton").onclick= changeText; //Don't include () here
+
+    // bind the element with id 'secondButton' to on click event
+    document.getElementById("secondButton").onclick = DynamicChange;
+
+}
+
+function DynamicChange()
+{
+    console.log("Inside Dynamic change function");
+
+    //let paragraphText = "<p> This is my dynamic paragraph </p>";
+    
+    let table = "<table>";
+            table += "<tr>";
+                table += "<th> Student Name </th>";
+                table += "<th> Email";
+            table += "</tr>";
+            table += "<tr>";
+                table += "<td> Harsimran </th>";
+                table += "<td> harsimran@nait.ca";
+            table += "</tr>";
+    table += "</table>";
+
+    //Now we have para created- lets add it to dynamic container before snow starts
+    let divElement = document.getElementById('dynamicPart');
+    // Using innerHTML you can add it there
+    divElement.innerHTML = table;
+    
 }
 
 function changeText()
 {
     console.log("Inside changeText function");
 
-    let paraText = prompt("Enter new text paragraph", "Text");
+    paraText = prompt("Enter new text paragraph", "Text");
     //Putting an entry into console log for varification
     console.log(paraText);
     // checking that value should not be a null value
@@ -36,7 +64,11 @@ function changeText()
     { // if is not null use that text to replace the value
         // Change it to any other text
         // innerHTML will change the HTML value of any element
-        document.getElementById("para1").innerHTML = paraText;
+        document.getElementById("para1").innerHTML += paraText;
+
+        // Shortcut way 
+
+        document.getElementById("para1").innerHTML = document.getElementById("para1").innerHTML + " " + paraText;
     }
     else
     {
